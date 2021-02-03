b6bd4c39eda529082a2eb3042146e9966ab13d8a
Nada61 <33845869+Nada61@users.noreply.github.com>
2021-02-03 19:54:52 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    /*Ady ll button class 3shan keda hy3ml 3la ay button*/
+    $("button").click(function(){
+        $("body").css("overflow" , "hidden");
+        /*var id =$(this).attr('custom');*/
+        $('.bk-soft').fadeIn();
+    });
+    $(".bk-soft img").click(function(event){
+        event.stopPropagation();
+        
+    });
+    $(".bk-soft").click(function(){
+        $("body").css("overflow" , "auto");
+        $(this).fadeOut();
+    });
+    
+    $(".form-data form").submit(function(e){
+        var error=0;
+        console.log(error);
+        e.preventDefault();
+
+        var inputs = $(this).find("input:not(.btn-primary)");
+        for(var i=0 ; i<inputs.length;i++){
+            if($(inputs[i]).val().length <5){
+                error++;
+            }
+        }
+        if(error>0){
+            e.preventDefault();
+            alert("Please Enter Data correctly");
+        }
+    });
+})
